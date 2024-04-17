93dc659f3b1b4819fc9b91ee6456f8291cc8f557
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-04-17 15:24:49 +0200

    feat: add score part

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
-let resultMessage = document.querySelector('.message').textContent;
+let resultMessage = 'Start guessing...';
 // write the rule
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
-
+// declare the score is staeting with 20
+let score = 20;
 // add click event to the check button to start game
 document.querySelector('.check').addEventListener('click', function () {
   // turn value of input from string to number
@@ -12,11 +13,17 @@ document.querySelector('.check').addEventListener('click', function () {
   // give all result after click on check button
   if (!guess) {
     resultMessage = '🚫 No number!';
-  } else if (guess === secretNumber){
-    resultMessage = '🎉 Correct number!'
-  } else if (guess < secretNumber){
-    resultMessage = '📉 Too low!'
-  } else if ( guess > secretNumber){
+  } else if (guess === secretNumber) {
+    resultMessage = '🎉 Correct number!';
+  } else if (guess < secretNumber) {
+    resultMessage = '📉 Too low!';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
     resultMessage = '📈 Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
+  // Update the text content of the ".message" element with the updated resultMessage
+  document.querySelector('.message').textContent = resultMessage;
 });

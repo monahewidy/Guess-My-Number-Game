2c0cba1f8f41e9750b59bbfba216e896361e148b
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-04-17 17:46:44 +0200

    feat: add style

--- script.js
+++ script.js
@@ -11,27 +11,35 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   // console.log(guess, typeof guess);
   // give all result after click on check button
+  // when there no input
   if (!guess) {
     resultMessage = '🚫 No number!';
-  } else if (guess === secretNumber) {
+  }
+  // when player wins
+  else if (guess === secretNumber) {
     resultMessage = '🎉 Correct number!';
-  } else if (guess < secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+  }
+  // when guess too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       resultMessage = '📉 Too low!';
       score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
-      (resultMessage = 'You lost the game!'),
-        (document.querySelector('.score').textContent = 0);
+      resultMessage = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
     }
-  } else if (guess > secretNumber) {
+  }
+  // when guess too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       resultMessage = '📈 Too high!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      (resultMessage = 'You lost the game!'),
-        (document.querySelector('.score').textContent = 0);
+      resultMessage = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
     }
   }
   // Update the text content of the ".message" element with the updated resultMessage

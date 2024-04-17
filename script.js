a0bdb076b81caa9b3a8f5a9926611b028584c6d6
Mona Hewidy <monaahmedhewidy@gmail.com>
2024-04-17 10:08:21 +0200

    feat: add click event

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+let resultMessage = document.querySelector('.message').textContent;
+// write the rule
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+// add click event to the check button to start game
+document.querySelector('.check').addEventListener('click', function () {
+  // turn value of input from string to number
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess);
+});
